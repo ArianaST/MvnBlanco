c48519b421f4bdcec52a85769cd44c4ef86eb819
ArianaST <marinestrellita@hotmail.com>
2021-12-13 15:23:42 -0600

    calificación corregido

--- practicas-app/aprobado.js
+++ practicas-app/aprobado.js
@@ -3,7 +3,7 @@ btnApobado.addEventListener("click", () => {
     axios.get("http://localhost:4567/aprobado", {
     })
         .then(function (res) {
-            window.location.replace("aprobado.html");
+            window.location.replace("pasado.html");
         })
         .catch(function (error) {
             console.log(error)
