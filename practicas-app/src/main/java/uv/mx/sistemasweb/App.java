3c25f1874285709e769d0e18e1f4e87301a00ec1
ArianaST <marinestrellita@hotmail.com>
2021-12-13 15:20:40 -0600

    calificaciones

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -1,13 +1,32 @@
 package uv.mx.sistemasweb;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/aprobado", (req, res) -> {
+            return "Aprobado";
+        });
     }
 }
